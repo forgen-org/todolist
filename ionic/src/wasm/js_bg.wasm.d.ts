7d6afc606a9bc14c1143c1979e1d19d731f3b3ac
Alexandre Hanot <>
2023-10-09 21:26:02 +0200

    add wasm to git

---
+++ ionic/src/wasm/js_bg.wasm.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function createTask(a: number, b: number): number;
+export function setPanicHook(): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h3ac111d77b4a2265(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__hafb8b5acdecc8f51(a: number, b: number, c: number, d: number): void;
+export function __wbindgen_free(a: number, b: number, c: number): void;
